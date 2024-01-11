a2153b8ee442f4d535964643e8fc1c17818bb903
root <root@stan>
2024-01-11 23:44:58 +0300

    fix error

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+"""function that returns a float sum of two variables"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    returns summation of two variables
+    parameters:
+        a[float]:
+            number to be added
+        b[float]:
+            another number
+        return:
+            [float]
+            return float
+    """
     return a + b
