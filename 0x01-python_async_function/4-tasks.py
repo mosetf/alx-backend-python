b468f6095746c7b9891407bce40e1b1489f2e967
mosetf <stanlym315@gmail.com>
2024-01-16 01:06:12 +0300

    use task_wait_random

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""4. Tasks"""
+import asyncio
+from typing import List
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """[summary]
+
+    Args:
+        n (int): [number of times wait_random will be called]
+        max_delay (int): [maximun value of delay]
+
+    Returns:
+        List[float]: [list of all the delays (float values)]
+    """
+    task_wait_random = __import__('3-tasks').task_wait_random
+    list_delays = []
+    for i in range(n):
+        list_delays.append(task_wait_random(max_delay))
+    return [await delay for delay in asyncio.as_completed(list_delays)]
