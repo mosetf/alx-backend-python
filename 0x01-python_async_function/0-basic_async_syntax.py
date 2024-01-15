cae5e1e20ec5ee01dfb03883013c2301351fc2a8
mosetf <stanlym315@gmail.com>
2024-01-15 18:27:25 +0300

    update4 Coroutine that waits for a random delay between 0 and max_delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10)-> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Coroutine that waits for a random delay between 0 and max_delay
     Args:
         max_delay (int, optional): [maximun value of delay]. Defaults to 10.
