9d9be882b74527a1ce165c06001d542f1acd09ee
mosetf <stanlym315@gmail.com>
2024-01-16 15:39:34 +0300

    Run time for four parallel comprehensions fix error 1

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,9 +2,9 @@
 """
 Run time for four parallel comprehensions
 """
-async_comprehension = __import__('1-async_comprehension').async_comprehension
 import asyncio
 import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
