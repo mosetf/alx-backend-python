9007039d9b4519607b7c6bf3cffdd376760ab2f2
mosetf <stanlym315@gmail.com>
2024-01-16 13:32:03 +0300

    Asynchronous generator that yields random numbers between 0 and 10. update 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,5 +10,5 @@ async def async_generator():
     Asynchronous generator that yields random numbers between 0 and 10.
     """
     for _ in range(10):
-        time.sleep(1)
+        asyncio.sleep(1)
         yield random.uniform(0, 10)
